426944928f39cb17b6a22c9dc5b548f2cab173c9
rlarbals <rlarbals4420@gmail.com>
2026-05-06 21:30:47 +0900

    안정적인 로그인 기능 구현완료.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -328,6 +328,7 @@ export default function App() {
                     <input
                       type="email"
                       placeholder="student@example.com"
+                      onChange={(e) => setEmail(e.target.value)}
                       className="w-full px-4 py-2.5 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent outline-none transition text-sm"
                     />
                   </div>
@@ -336,6 +337,7 @@ export default function App() {
                     <input
                       type="password"
                       placeholder="••••••••"
+                      onChange={(e) => setPassword(e.target.value)}
                       className="w-full px-4 py-2.5 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent outline-none transition text-sm"
                     />
                   </div>
